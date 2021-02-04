6521a291c57f3e60f2662654dc63806800a8c108
dasilva_ricardo <dasilvaricardog@gmail.com>
2021-02-04 20:02:35 -0300

    .dsad

--- script_funciones.js
+++ script_funciones.js
@@ -10,9 +10,11 @@ function buildProductoCard(producto){
     let div = ` <div class="card" style="width: 18rem;">
     <img src="..." class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+      <h5 class="card-title">${marca}</h5>
+      <h5 class="card-title">${tipo}</h5>
+      <h5 class="card-title">${precio}</h5>
+      
+      <a href="#" class="btn btn-primary">COMPRAR</a>
     </div>
   </div>
     `
