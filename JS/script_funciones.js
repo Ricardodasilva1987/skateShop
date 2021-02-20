35514c5e2eac704d34b8ba2826de48aa067695fc
dasilva_ricardo <dasilvaricardog@gmail.com>
2021-02-20 17:05:07 -0300

    fix rute Images

--- JS/script_funciones.js
+++ JS/script_funciones.js
@@ -1,15 +1,15 @@
 //se crearon objetos un objeto por producto
-var baker = new MyProduct(1, "Board", "Baker", 10000, "/Images/baker.jpg");
-var santaCruz = new MyProduct(2, "Board", "Santa Cruz", 18000, "/Images/santacruzsakeboards.jpg");
-var shortys = new MyProduct(3, "Board", "Shortys", 15000, "/Images/shortys.jpg");
-var blind = new MyProduct(4, "Board", "Blind", 5000, "/Images/blindskate.jpg");
-var independent = new MyProduct(5, "Trucks", "Independent", 15000, "/Images/independenttruck.jpg");
-var destructo = new MyProduct(6, "Trucks", "destructo", 25000, "/Images/destructotrucks.jpg");
-var tensor = new MyProduct(7, "Trucks", "Tensor", 25000, "/Images/tensortruck.jpg");
-var phantom = new MyProduct(8, "Trucks", "Phamton", 25000, "/Images/phamtontrucks.jpg");
-var pigw = new MyProduct(9, "Wheels", "Pig", 3000, "/Images/pigwheels.jpg");
-var spitfire = new MyProduct(10, "Wheels", "Spitfire", 5000, "/Images/sfwheels.jpg");
-var powell = new MyProduct(11, "Wheels", "Powell Peralta", 5000, "/Images/ppwheels_.jpg");
+var baker = new MyProduct(1, "Board", "Baker", 10000, "Images/baker.jpg");
+var santaCruz = new MyProduct(2, "Board", "Santa Cruz", 18000, "Images/santacruzsakeboards.jpg");
+var shortys = new MyProduct(3, "Board", "Shortys", 15000, "Images/shortys.jpg");
+var blind = new MyProduct(4, "Board", "Blind", 5000, "Images/blindskate.jpg");
+var independent = new MyProduct(5, "Trucks", "Independent", 15000, "Images/independenttruck.jpg");
+var destructo = new MyProduct(6, "Trucks", "destructo", 25000, "Images/destructotrucks.jpg");
+var tensor = new MyProduct(7, "Trucks", "Tensor", 25000, "Images/tensortruck.jpg");
+var phantom = new MyProduct(8, "Trucks", "Phamton", 25000, "Images/phamtontrucks.jpg");
+var pigw = new MyProduct(9, "Wheels", "Pig", 3000, "Images/pigwheels.jpg");
+var spitfire = new MyProduct(10, "Wheels", "Spitfire", 5000, "Images/sfwheels.jpg");
+var powell = new MyProduct(11, "Wheels", "Powell Peralta", 5000, "Images/ppwheels_.jpg");
 
 
 
