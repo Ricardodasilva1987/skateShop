3d959c55727bc90735d4de8136667c64d44e30e2
dasilva_ricardo <dasilvaricardog@gmail.com>
2021-02-20 16:50:18 -0300

    1er commit

---
+++ JS/script.js
@@ -0,0 +1,121 @@
+//SE CREA CARRITO COMO UNA FUNCION CONSTRUCTORA
+
+function MyShopCar() {
+
+    //se crea esta propiedad, que sera un array de los productos agregados
+    this.compras = []
+    
+    //METODOS
+
+    //se crea un metodo para ir integrando los objetos a la propiedad compra.
+    this.addItem = function (compra) {
+        this.compras.push(compra);
+        
+    }
+    //Se crea otro metodo para sumar el total
+    this.totalShop = function () {
+        let sumaTotal = 0;
+        for (let i = 0; i < this.compras.length; i++) {
+            let compra = this.compras[i];
+            sumaTotal += compra.producto.precio * compra.cantidad;
+        }
+
+        return sumaTotal;
+
+    }
+
+    this.inicializarCart = function(){
+       
+            if (sessionStorage.getItem("Carrito") != null) {
+              this.compras = JSON.parse(sessionStorage.getItem("Carrito"));
+              console.log(this.compras);
+              actualizarTablaCompra();
+            }
+        }
+    
+
+    //
+    //se crea  metodo para ir sumar el total de cantidad
+
+    this.totalQuantity = function () {
+        let sumaCantidad = 0;
+        for (let i = 0; i < this.compras.length; i++) {
+            let compra = this.compras[i];
+            sumaCantidad += compra.cantidad;
+        }
+
+        return sumaCantidad;
+
+        //se crea metodo para borrar el carrito
+    }
+    this.eraseCartShop = function () {
+        while (this.compras.length > 0) {
+            this.compras.pop();
+        }
+        sessionStorage.clear();
+    }
+
+    this.comprar = function (id, cantidad = 1) {
+        let found = false;
+        for (let compra of this.compras) {
+            if (compra.producto.id == id) {
+                compra.cantidad += cantidad;
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            const product = buscarProducto(id);
+
+            //   const product = allProducts.find(function (p) {
+            //    if(p.id==id){
+            //        return true;
+            //    }
+            //    else {
+            //        return false;
+            //    }
+
+
+
+            // return (p.id == id);
+            //  });
+            // console.log(product);
+            cart.addItem(new Compra(product, 1));
+            cart.totalShop();
+            cart.totalQuantity();
+            sessionStorage.setItem("Carrito", JSON.stringify(this.compras));
+
+
+
+        }
+
+        actualizarTablaCompra();
+        
+
+    }
+
+
+
+}
+//se creo objeto carrito
+var cart = new MyShopCar();
+
+
+//Se crea una funcion constructora de la accion de compra
+function Compra(producto, cantidad) {
+    this.producto = producto;
+    this.cantidad = cantidad;
+
+}
+
+//SE CREA FUNCION CONSTRUCTORA del producto
+
+function MyProduct(id, tipo, marca, precio, img) {
+    this.id = id;
+    this.tipo = tipo;
+    this.marca = marca;
+    this.precio = precio;
+    this.img = img;
+
+
+}
--- JS/script_funciones.js
+++ JS/script_funciones.js
@@ -20,6 +20,11 @@ let wheelsArr = [pigw, spitfire, powell];
 //SE UNIERON CON UN CONCAT EN UN SOLO ARRAY EMULANDO UN JSON
 let allProducts = [].concat(boardArr, truckArr, wheelsArr);
 
+//Se crea un JSON
+let allproductsjs = JSON.stringify(allProducts);
+// console.log(allproductsjs);
+// console.log(JSON.parse(allproductsjs));
+
 
 
 //INICIALAZAMOS VARIABLES del DOM
@@ -29,24 +34,25 @@ let productTruck = document.getElementById('product-truck');
 let productWheels = document.getElementById('product-wheels');
 let carritoList = document.getElementById('productos-cart');
 let titulo = document.getElementById('titulo-tablas');
-let tituloTruck = document.getElementById('titulo-truck');
 let totalShopFooter = document.getElementById('total-shop');
 let borrarCompra = document.getElementById('erase');
-let adquirirCompra =document.getElementById('shop')
-let modal=document.getElementById('modal');
+let adquirirCompra = document.getElementById('shop')
+let modal = document.getElementById('modal');
 let titulomodal = document.getElementById('titulomodal');
+let usuario = document.getElementById('usuario');
 
 
 
-//Eventos
-titulo.onclick = function (event) {
-    console.log(event);
-};
-tituloTruck.onmouseout = function (event) {
-    console.log(event);
-}
-
-
+//Eventos JQUERY
+//PRUEBA
+let tituloTruck = $("#titulo-truck").click(function () {
+    $("#titulo-truck").hide();
+})
+//BOTON DE BORRADO y COMPRAR
+let erase = $("#erase").click(eraseTabla);
+let mostrarcompra = $("#mostrarcompra").click(mostrarCompra);
+let ingresoUSer = $("#usuario").click(ingresoDeUsuario);
+//
 
 //DESARROLLO DE LA METODOLOGIA
 
@@ -66,13 +72,13 @@ function showProduct(listaDeProductos, itemType) {
 
 function buildProductoCard(producto) {
     let div = ` <div class="card" style="width: 15rem;">
-    <img src="${producto.img}" class="card-img-top" alt="...">
+    <img src="${producto.img}" class="card-img-top" alt="productos carrito">
     <div class="card-body">
       <h5 class="card-title">${producto.marca}</h5>
       <h5 class="card-title">${producto.tipo}</h5>
       <h5 class="card-title">${producto.precio}</h5>
-      
-      <button class="btn btn-dark comprar" onclick='comprar(${producto.id})' >COMPRAR</button>
+
+      <button class="btn btn-dark comprar" onclick='cart.comprar(${producto.id})' >COMPRAR</button>
     </div>
   </div>
     `
@@ -80,57 +86,75 @@ function buildProductoCard(producto) {
 }
 
 //funcion que capta con un evento el producto y lo guarda en la consola
-function comprar(id) {
-    const product = allProducts.find(function (p) {
-        return (p.id == id);
-
-
-       
-    });
-    // console.log(product);
-    cart.addItem(new Compra(product, 1));
-    cart.totalShop();
-    cart.totalQuantity();
-
-    actualizarTablaCompra();
-
-    
+function buscarProducto(id) {
+    for (let product of allProducts) {
+        if (product.id == id)
+            return product
+    }
+    return null;
 }
+
+// function comprar(id) {
+//     const product = buscarProducto(id);
+
+//     //   const product = allProducts.find(function (p) {
+//     //    if(p.id==id){
+//     //        return true;
+//     //    }
+//     //    else {
+//     //        return false;
+//     //    }
+
+
+
+//         // return (p.id == id);
+//     //  });
+//     // console.log(product);
+//     cart.addItem(new Compra(product, 1));
+//     cart.totalShop();
+//     cart.totalQuantity();
+
+//     actualizarTablaCompra();
+
+
+// }
 
 function actualizarTablaCompra() {
     let html = '';
-    let html2='';
+    let html2 = '';
     for (let compra of cart.compras) {
         // console.log(compra.producto.precio)
         // console.log(compra.producto.marca)
         html += `<tr>
         <th scope="row">${compra.producto.id}</th>
+        <th scope="row">${compra.producto.tipo}</th>
         <td>${compra.producto.marca}</td>
         <td>$ ${compra.producto.precio}</td>
         <td>${compra.cantidad}</td>
       </tr>`
-       
-        html2=`<tr>
+
+        html2 = `<tr>
         <th scope="row">'------'</th>
+        <td>TOTAL</td>
         <td>TOTAL</td>
         <td>$ ${cart.totalShop()}</td>
         <td>${cart.totalQuantity()}</td>
       </tr>`
-        
+
     }
-     //Definiendo donde entraran las filas creadas en actualizarTablaCompra
+    //Definiendo donde entraran las filas creadas en actualizarTablaCompra
     carritoList.innerHTML = html;
-    totalShopFooter.innerHTML=html2;
+    totalShopFooter.innerHTML = html2;
     // console.log(`El total de la compra es`, cart.totalShop())
     // console.log(cart.totalQuantity());
-    
+
 }
 
-function eraseTabla () {
+function eraseTabla() {
     cart.eraseCartShop();
 
-    carritoList.innerHTML= '';
-    totalShopFooter.innerHTML=`<tr>
+    carritoList.innerHTML = '';
+    totalShopFooter.innerHTML = `<tr>
     <th scope="row">'------'</th>
     <td>'-------'</td>
     <td>$ 0</td>
@@ -138,20 +162,29 @@ function eraseTabla () {
   </tr>`
 }
 
-function mostrarCompra () {
-    if(cart.totalShop()>0){
-        modal.innerHTML=`<p> Haz ha adquirido ${cart.totalQuantity()} items
+function mostrarCompra() {
+    if (cart.totalShop() > 0) {
+        modal.innerHTML = `<p> Haz ha adquirido ${cart.totalQuantity()} items
         por un monto total de $ ${cart.totalShop()} pesos</p> `
-        titulomodal.innerHTML=` <h3 class="modal-title" id="exampleModalLabel">Lets roll !!!</h3> `
-       
-    }
-    else{
-        modal.innerHTML=`<p> No has comprado nada POSER !!!</p> `
-        titulomodal.innerHTML=` <h3 class="modal-title" id="exampleModalLabel">Estas cagao Sapo ? !!!</h3> `
+        titulomodal.innerHTML = ` <h3 class="modal-title" id="exampleModalLabel">Lets roll !!!</h3> `
+
+    } else {
+        modal.innerHTML = `<p> No has comprado aun</p> `
+        titulomodal.innerHTML = ` <h3 class="modal-title" id="exampleModalLabel">Tienes el carrito vacio</h3> `
     }
 
 
 }
+
+function ingresoDeUsuario() {
+        cart.inicializarCart();
+    }
+    
+
+
+
+
+
 
 
 
@@ -163,4 +196,3 @@ showProduct(wheelsArr, productWheels);
 
 
 console.log(cart.compras);
-
