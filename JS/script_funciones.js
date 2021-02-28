e3db7671461773fbf13fa1739f38ed8b1c9abd58
dasilva_ricardo <dasilvaricardog@gmail.com>
2021-02-28 14:37:36 -0300

    modificacion jquery

--- JS/script_funciones.js
+++ JS/script_funciones.js
@@ -1,48 +1,27 @@
-// //se crearon objetos un objeto por producto
-// var baker = new MyProduct(1, "Board", "Baker", 10000, "Images/baker.jpg");
-// var santaCruz = new MyProduct(2, "Board", "Santa Cruz", 18000, "Images/santacruzsakeboards.jpg");
-// var shortys = new MyProduct(3, "Board", "Shortys", 15000, "Images/shortys.jpg");
-// var blind = new MyProduct(4, "Board", "Blind", 5000, "Images/blindskate.jpg");
-// var independent = new MyProduct(5, "Trucks", "Independent", 15000, "Images/independenttruck.jpg");
-// var destructo = new MyProduct(6, "Trucks", "destructo", 25000, "Images/destructotrucks.jpg");
-// var tensor = new MyProduct(7, "Trucks", "Tensor", 25000, "Images/tensortruck.jpg");
-// var phantom = new MyProduct(8, "Trucks", "Phamton", 25000, "Images/phamtontrucks.jpg");
-// var pigw = new MyProduct(9, "Wheels", "Pig", 3000, "Images/pigwheels.jpg");
-// var spitfire = new MyProduct(10, "Wheels", "Spitfire", 5000, "Images/sfwheels.jpg");
-// var powell = new MyProduct(11, "Wheels", "Powell Peralta", 5000, "Images/ppwheels_.jpg");
 
 
 
-//SE CREAN ARREGLOS DE LOS PRODUCTOS POR CATEGORIA
-// let boardArr = [baker, shortys, blind, santaCruz];
-// let truckArr = [independent, destructo, phantom, tensor];
-// let wheelsArr = [pigw, spitfire, powell];
-//SE UNIERON CON UN CONCAT EN UN SOLO ARRAY EMULANDO UN JSON
-let allProducts = [];//.concat(boardArr, truckArr, wheelsArr);
+
+let allProducts = []; //.concat(boardArr, truckArr, wheelsArr);
 
 
 
-//INICIALAZAMOS VARIABLES del DOM
+//INICIALAZAMOS VARIABLES del DOM JQUERY
 
-let productBoard = document.getElementById('product-board');
-let productTruck = document.getElementById('product-truck');
-let productWheels = document.getElementById('product-wheels');
-let carritoList = document.getElementById('productos-cart');
-let titulo = document.getElementById('titulo-tablas');
-let totalShopFooter = document.getElementById('total-shop');
-let borrarCompra = document.getElementById('erase');
-let adquirirCompra = document.getElementById('shop')
-let modal = document.getElementById('modal');
-let titulomodal = document.getElementById('titulomodal');
-let usuario = document.getElementById('usuario');
+ 
+ let carritoList = document.getElementById('productos-cart');
+//  let titulo = document.getElementById('titulo-tablas');
+ let totalShopFooter = document.getElementById('total-shop');
+ let borrarCompra = document.getElementById('erase');
+ let adquirirCompra = document.getElementById('shop')
+ let modal = document.getElementById('modal');
+ let titulomodal = document.getElementById('titulomodal');
+ let usuario = document.getElementById('usuario');
 
 
 
-//Eventos JQUERY
-//PRUEBA
-let tituloTruck = $("#titulo-truck").click(function () {
-    $("#titulo-truck").hide();
-})
+
+
 //BOTON DE BORRADO y COMPRAR
 let erase = $("#erase").click(eraseTabla);
 let mostrarcompra = $("#mostrarcompra").click(mostrarCompra);
@@ -53,14 +32,17 @@ let ingresoUSer = $("#usuario").click(ingresoDeUsuario);
 
 //Creacion de productos de manera dinamica que recibiran como argumentos un array y un elementohtml
 
-function showProduct(listaDeProductos, itemType) {
-    //EL itemType es product-board; product-truck o product-wheels
-    itemType.innerHTML = ' ';
+function showProduct(listaDeProductos, selector) {
+    
+    let html = '';
+    
     //SE UTILIZA UN LOOP PARA RECORRER EL ARREGLO
     listaDeProductos.forEach(producto => {
         const card = buildProductoCard(producto);
-        itemType.innerHTML += card;
+        html += card;
     });
+
+    $(selector).html(html);
 }
 
 
@@ -115,10 +97,11 @@ function actualizarTablaCompra() {
 
     }
     //Definiendo donde entraran las filas creadas en actualizarTablaCompra
-    carritoList.innerHTML = html;
-    totalShopFooter.innerHTML = html2;
-    // console.log(`El total de la compra es`, cart.totalShop())
-    // console.log(cart.totalQuantity());
+    // carritoList.innerHTML = html;
+    $("#productos-cart").html(html);
+     totalShopFooter.innerHTML = html2;
+    // $("#total-shop").html(html2);
+  
 
 }
 
@@ -152,100 +135,8 @@ function ingresoDeUsuario() {
     cart.inicializarCart();
 }
 
-function getData(callback) {
-    setTimeout(function () {
-        const json = `{
-                    "boards":[
-                        {
-                        "id":1,
-                        "marca":"Baker",
-                        "tipo":"Boards",
-                        "precio":10000,
-                        "img":"Images/baker.jpg"     
-                        },
-                        {
-                        "id":2,
-                        "marca":"Santa Cruz",
-                        "tipo":"Board",
-                        "precio":18000,
-                        "img":"Images/santacruzsakeboards.jpg"   
-                        },
-                        {
-                        "id":3,
-                        "marca":"Shortys",
-                        "tipo":"Board",
-                        "precio":15000,
-                        "img":"Images/shortys.jpg"  
-                        },
-                        {
-                        "id":4,
-                        "marca":"Blind",
-                        "tipo":"Board",
-                        "precio":9500,
-                        "img":"Images/blindskate.jpg"   
-                        }
-                    ],
-                    "trucks":[
-                        {
-                        "id":5,
-                        "marca":"Independent",
-                        "tipo":"Trucks",
-                        "precio":15000,
-                        "img":"Images/independenttruck.jpg"
-                        },
-                        {
-                        "id":6,
-                        "marca":"Destructo",
-                        "tipo":"Trucks",
-                        "precio":14500,
-                        "img":"Images/destructotrucks.jpg"
-                        },
-                        {
-                        "id":7,
-                        "marca":"Tensor",
-                        "tipo":"Trucks",
-                        "precio":16500,
-                        "img":"Images/tensortruck.jpg"
-                        },
-                        {
-                        "id":8,
-                        "marca":"Phantom",
-                        "tipo":"Trucks",
-                        "precio":15500,
-                        "img":"Images/phamtontrucks.jpg"
-                        }
-                        ],
-                    "wheels":[
-                            {
-                        "id":9,
-                        "marca":"Pig",
-                        "tipo":"Wheels",
-                        "precio":5500,
-                        "img":"Images/pigwheels.jpg"
-                            },
-                            {
-                        "id":10,
-                        "marca":"Spitfire",
-                        "tipo":"Wheels",
-                        "precio":6250,
-                        "img":"Images/sfwheels.jpg"
-                        },
-                         {
-                        "id":11,
-                        "marca":"Powell Peralta",
-                        "tipo":"Wheels",
-                        "precio":7200,
-                        "img":"Images/ppwheels_.jpg"
-                        }
-                    ]
-             }`;
-        callback(json);
-    }, 2000)
-}
-
-function procesarData(json){
-    const data = JSON.parse(json);
-    console.log('llego la data', data);
+function procesarData(data){
+    
     
     for(let board of data.boards){
         allProducts.push(board);
@@ -261,24 +152,24 @@ function procesarData(json){
     console.log("allProducts: ", allProducts);
 
 
-    showProduct(data.boards, productBoard);
-    showProduct(data.trucks, productTruck);
-    showProduct(data.wheels, productWheels);
+    showProduct(data.boards, "#product-board");
+    showProduct(data.trucks, "#product-truck");
+    showProduct(data.wheels, "#product-wheels");
 }
 
-function init(){
-    getData(procesarData);
-}
 
-//Inicializo el renderizado a partir de un JSON .
-init();
+$(document).ready(function(){
+    $("#cart-btn").click(function(event){
+        $("#carrito").toggle(1000);
+    })
+    $.ajax({
+        url : "https://raw.githubusercontent.com/Ricardodasilva1987/skateShop/master/productos.json",
+        dataType:"json",
+        success: function(response){
+            console.log(response);
+            procesarData(response);
+        }
 
-//Llamo a las funciones para cargar el DOM con los productos
-// showProduct(boardArr, productBoard);
-// showProduct(truckArr, productTruck);
-// showProduct(wheelsArr, productWheels);
+    })
 
-
-// console.log(cart.compras);
-
-console.log(json)
+})
